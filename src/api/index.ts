947b1509ef806db9aaf6669ffbb01e7b93afb3ee
Wajeeh <swajeeh03@gmail.com>
2026-01-24 18:46:14 +0500

    Update

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { logout, setOdooAccessToken } from '../redux/slices/AuthSlice';
 import { store } from '../redux/store';
 export const AUTHORIZE = 'AUTHORIZE';
 export const NETWORK_ERROR = 'NETWORK ERROR';
-export const BASE_URL = 'https://teamsol-api-serverless.suncropgroup.com.pk';
+export const BASE_URL = 'https://api.chainsol-dev.suncropgroup.com.pk';
 import { toast } from 'sonner';
 
 export const Method = {
